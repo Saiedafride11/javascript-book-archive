c06bdba000d8db49a7ff516ab4aa13be7062253c
Coders Key <designer.afride@gmail.com>
2021-09-02 00:35:32 -0700

    error messege solve

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 /*----------------  ---------------------------*/
+const searchResult = document.getElementById('search-result');
+const showResults = document.getElementById('show-results');
+const noResults = document.getElementById('no-results');
 
 // spinner
 const toggleSpinner = displayStyle => {
@@ -6,49 +9,68 @@ const toggleSpinner = displayStyle => {
 }
 // search-result
 const toggleSearchResult = displayStyle => {
-    document.getElementById('search-result').style.display = displayStyle;
+    searchResult.style.display = displayStyle;
 }
 
 
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // spinner
-    toggleSpinner('block');
-    toggleSearchResult('none');
-    searchField.value = '';
+    
+    searchResult.innerHTML = ''
    
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    if(searchText == ''){
+        showResults.innerText = 'Plese write something to display';
+        noResults.innerText = '';
+        searchResult.innerHTML = ''
+    }
+    else{
+        showResults.innerText = '';
+        noResults.innerText = '';
+         // spinner
+        toggleSpinner('block');
+        toggleSearchResult('none');
+        searchField.value = '';
+
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
         .then(response => response.json())
-        .then(data => displaySearchResult(data.docs))
+        .then(data => displaySearchResult(data))
+
+        .catch(error => console.log(error))
+
+    }
 }
 
 
 const displaySearchResult = (books) => {
-    document.getElementById('total-search-result').innerText = 'Search results found: ' + books.length;
+    document.getElementById('total-search-result').innerText = books.numFound;
+    document.getElementById('search').style.display = 'block';
 
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-    
-    books?.forEach(book => {
-        console.log(book);
-        const div = document.createElement('div');
-        // div.style.boxShadow = '0 1px 1px gray';
-        div.classList.add('col');
-        div.innerHTML = `
-            <div onclick="" class="card h-100">
-                <img height="300px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h4 class="card-title text-primary">${book.title}</h4>
-                    <p class="card-text">Author: <strong>${book.author_name ? book.author_name : '----------'}</strong></p>
-                    <p class="card-text">Publisher: <strong>${book.publisher ? book.publisher : '----------'}</strong></p>
-                    <p class="card-text">Published Year: <strong>${book.first_publish_year ? book.first_publish_year : '----------'}</strong></p>
+    if(books.numFound == 0){
+        noResults.innerText = 'Show no more Result found';
+    }
+    else{
+        searchResult.innerHTML = ''
+
+         books.docs?.forEach(book => {
+            // console.log(book);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card h-100" style="box-shadow:0 10px 20px gray;">
+                    <img height="300px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title text-primary">${book.title}</h4>
+                        <p class="card-text">Author: <strong>${book.author_name ? book.author_name : '----------'}</strong></p>
+                        <p class="card-text">Publisher: <strong>${book.publisher ? book.publisher : '----------'}</strong></p>
+                        <p class="card-text">Published Year: <strong>${book.first_publish_year ? book.first_publish_year : '----------'}</strong></p>
+                    </div>
                 </div>
-            </div>
-        `
-        searchResult.appendChild(div);
-    })
-    toggleSpinner('none');
-    toggleSearchResult('flex');
+            `
+            searchResult.appendChild(div);
+        })
+    }
+        toggleSpinner('none');
+        toggleSearchResult('flex');
 }
